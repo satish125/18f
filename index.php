1883ebcc12aa3032c0734dfe382939f4793df6ba
daniel.pegues <daniel.pegues@ntconcepts.com>
2015-06-19 10:28:22 -0400

    Adding initial files and folder structure for frontend splash page.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCYTPE html>
+<html language="en">
+<head>
+	<title>18F Prototype Homepage</title>
+</head>
+<body>
+
+Development starting. This page will be used for the homepage.
+
+</body>
+</html>
