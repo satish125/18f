b88d0ee976fb9b907898a3d46fc9a5afddc5831f
daniel.pegues <daniel.pegues@ntconcepts.com>
2015-07-01 14:41:18 -0400

    Adding update to feeds.php to repo for use by others in the team.

--- inc/feeds.php
+++ inc/feeds.php
@@ -4,8 +4,24 @@
 <?php /* Top 5 Info: Start */ ?>
 <div class="col-sm-6 top5info">
 	
-	
-	
+	<table class="recalltable">
+		<thead>
+			<tr>
+				<td>City</td>
+				<td>State</td>
+				<td>Recalling Firm</td>
+				<td>Date</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>New Orleans</td>
+				<td>LA</td>
+				<td>CVS</td>
+				<td>2015-12-21</td>
+			</tr>
+		</tbody>
+	</table>
 	
 	<div class="clear"></div>
 </div>
@@ -18,4 +34,3 @@
 	<div class="clear"></div>
 </div>
 <?php /* Twitter: End */ ?>
-							
